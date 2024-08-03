9194e74715fc60e19ba372edff152345e13300c3
abrilxcx <abrlbjrn@gmail.com>
2024-08-03 14:12:02 -0600

    First commit

---
+++ src/Animazing.jsx
@@ -0,0 +1,5 @@
+export const Animazing = () => {
+   return (
+      <div>Animazing</div>
+   )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import { Animazing } from './Animazing';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Animazing />
+  </React.StrictMode>,
+)
