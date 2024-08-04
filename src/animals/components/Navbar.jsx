3a54bec49075e961e74aa656f3aad0236785165d
abrilxcx <abrlbjrn@gmail.com>
2024-08-04 17:09:05 -0600

    Made minor update

--- src/animals/components/Navbar.jsx
+++ src/animals/components/Navbar.jsx
@@ -40,7 +40,7 @@ export const Navbar = () => {
          <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ps-3">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link">
-                        Abril
+                        Liss
                     </span>
 
                     <button
