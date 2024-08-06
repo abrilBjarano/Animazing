c0aff4cbefe54563c82805056072cb9997ee466b
abrilxcx <abrlbjrn@gmail.com>
2024-08-06 13:56:04 -0600

    Added Search and ANimal page and implemented in Routes config and navbar

--- src/animals/hooks/useAnimalsRoutesInfo.jsx
+++ src/animals/hooks/useAnimalsRoutesInfo.jsx
@@ -3,6 +3,7 @@ export const useAnimalsRoutesInfo = () => {
    const routes = [
       { name: 'Terrestrial', path: 'terrestrial' },
       { name: 'Marine', path: 'marine' },
+      { name: 'Search', path: 'search' },
    ]
 
 
--- src/animals/routes/AnimalsRoutes.jsx
+++ src/animals/routes/AnimalsRoutes.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import { TerrestrialPage } from "../pages/TerrestrialPage";
 import { MarinePage } from "../pages/MarinePage";
+import { SearchPage } from "../pages/SearchPage";
+import { AnimalPage } from "../pages/AnimalPage";
 
 
 export const AnimalsRoutes = () => {
@@ -13,6 +15,9 @@ export const AnimalsRoutes = () => {
          <Routes>
             <Route path="terrestrial" element={ <TerrestrialPage /> } />
             <Route path="marine" element={ <MarinePage /> } />
+
+            <Route path="search" element={ <SearchPage /> } />
+            <Route path="animal" element={ <AnimalPage /> } />
 
             <Route path="/*" element={ <Navigate to="terrestrial" /> } />
          </Routes>
