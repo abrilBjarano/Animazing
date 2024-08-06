3edf4b681fb3687704bf3db0fb46c12ba1dfd012
abrilxcx <abrlbjrn@gmail.com>
2024-08-06 12:00:05 -0600

    Added Login page to AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { MarinePage } from "../animals/pages/MarinePage";
 import { TerrestrialPage } from "../animals/pages/TerrestrialPage";
+import { LoginPage } from "../auth/pages/LoginPage";
 
 
 export const AppRouter = () => {
    return (
-      <Routes>
-         <Route path="terrestrial" element={ <TerrestrialPage /> } />
-         <Route path="marine" element={ <MarinePage /> } />
+      <>
+         <Routes>
+            <Route path="terrestrial" element={ <TerrestrialPage /> } />
+            <Route path="marine" element={ <MarinePage /> } />
 
-         <Route path="/*" element={ <Navigate to="terrestrial" /> } />
-      </Routes>
+            <Route path="login" element={ <LoginPage /> } />
+
+            <Route path="/*" element={ <Navigate to="terrestrial" /> } />
+         </Routes>
+      </>
    )
 }
