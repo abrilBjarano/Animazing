03b3226af73191864661de8d7b08a1d9f4198fee
abrilxcx <abrlbjrn@gmail.com>
2024-08-07 13:24:59 -0600

    Added data for animals

---
+++ src/animals/data/animals.js
@@ -0,0 +1,142 @@
+const animals = [
+   {
+       'id': 'marino-caballito',
+       'name': 'Caballito de mar',
+       'habitat': 'Marino',
+       'species': 'Hippocampus',
+       'description': 'Los caballitos de mar son pequeños peces conocidos por su apariencia única y su forma de nadar en posición vertical. Habitan en mares tropicales y templados alrededor del mundo. Los caballitos de mar son monógamos y forman lazos fuertes con su pareja. Son conocidos por la característica inusual de que el macho lleva los huevos en una bolsa especial hasta que eclosionan.'
+   },
+   {
+       'id': 'marino-cocodrilo',
+       'name': 'Cocodrilo',
+       'habitat': 'Marino',
+       'species': 'Crocodylus',
+       'description': 'Los cocodrilos son reptiles grandes y poderosos que viven en hábitats de agua dulce y salada. Son conocidos por su piel gruesa y su poderoso mordisco. Los cocodrilos tienen una mordida extremadamente fuerte y son depredadores apex en su entorno. A pesar de su apariencia feroz, los cocodrilos son importantes para el ecosistema, ya que ayudan a controlar las poblaciones de peces y otros animales acuáticos.'
+   },
+   {
+       'id': 'marino-delfin',
+       'name': 'Delfín',
+       'habitat': 'Marino',
+       'species': 'Delphinidae',
+       'description': 'Los delfines son mamíferos marinos conocidos por su inteligencia y comportamiento social. Viven en océanos y mares de todo el mundo y se comunican mediante sonidos y ecolocación. Los delfines son extremadamente sociales y viven en grupos llamados manadas. Tienen una gran capacidad para aprender y realizar trucos, y son conocidos por su comportamiento juguetón y su interacción con los humanos.'
+   },
+   {
+       'id': 'marino-estrella',
+       'name': 'Estrella de mar',
+       'habitat': 'Marino',
+       'species': 'Asteroidea',
+       'description': 'Las estrellas de mar son equinodermos con brazos radiales que habitan en el fondo marino. Pueden regenerar sus extremidades perdidas y juegan un papel importante en el ecosistema marino. Las estrellas de mar tienen la capacidad única de regenerar partes de su cuerpo, lo que les permite sobrevivir a depredadores y lesiones. Son importantes para mantener el equilibrio de los ecosistemas marinos al controlar las poblaciones de otros organismos.'
+   },
+   {
+       'id': 'marino-foca',
+       'name': 'Foca',
+       'habitat': 'Marino',
+       'species': 'Pinnipedia',
+       'description': 'Las focas son mamíferos acuáticos que habitan en regiones frías y templadas. Son conocidas por su habilidad para nadar y su comportamiento social en colonias. Las focas son nadadoras ágiles y pueden sumergirse a grandes profundidades en busca de alimento. Forman grandes colonias en las costas y son animales sociales que se comunican mediante vocalizaciones y gestos.'
+   },
+   {
+       'id': 'marino-nutria',
+       'name': 'Nutria',
+       'habitat': 'Marino',
+       'species': 'Lutrinae',
+       'description': 'Las nutrias son mamíferos semiacuáticos conocidos por su habilidad para nadar y su comportamiento juguetón. Viven en cuerpos de agua dulce y en algunas regiones costeras. Las nutrias son muy inteligentes y utilizan herramientas, como piedras, para abrir conchas y acceder a su alimento. Son animales sociales que disfrutan jugando y son esenciales para la salud de los ecosistemas acuáticos.'
+   },
+   {
+       'id': 'marino-orca',
+       'name': 'Orca',
+       'habitat': 'Marino',
+       'species': 'Orcinus orca',
+       'description': 'Las orcas, también conocidas como ballenas asesinas, son mamíferos marinos grandes y poderosos. Son depredadores apex y tienen una dieta variada que incluye peces y mamíferos marinos. Las orcas son altamente inteligentes y tienen estructuras sociales complejas. Viven en grupos familiares llamados pods y utilizan estrategias de caza cooperativa para capturar a sus presas.'
+   },
+   {
+       'id': 'marino-pinguino',
+       'name': 'Pingüino',
+       'habitat': 'Marino',
+       'species': 'Spheniscidae',
+       'description': 'Los pingüinos son aves no voladoras que viven principalmente en el hemisferio sur. Son conocidos por su habilidad para nadar y su vida en colonias grandes. Los pingüinos son expertos nadadores y pueden sumergirse a profundidades considerables para cazar peces y otros alimentos marinos. Forman grandes colonias en las costas, donde crían a sus polluelos y se protegen mutuamente de los depredadores.'
+   },
+   {
+       'id': 'marino-tiburon',
+       'name': 'Tiburón',
+       'habitat': 'Marino',
+       'species': 'Selachimorpha',
+       'description': 'Los tiburones son peces grandes y carnívoros que habitan en océanos de todo el mundo. Son conocidos por sus dientes afilados y su papel como depredadores apex. Los tiburones tienen sentidos agudos y son eficaces cazadores. Desempeñan un papel crucial en el mantenimiento de la salud de los océanos al controlar las poblaciones de otras especies marinas.'
+   },
+   {
+       'id': 'marino-tortuga',
+       'name': 'Tortuga marina',
+       'habitat': 'Marino',
+       'species': 'Cheloniidae',
+       'description': 'Las tortugas marinas son reptiles que pasan la mayor parte de su vida en el océano. Son conocidas por sus largas migraciones y su capacidad para navegar grandes distancias. Las tortugas marinas son importantes para los ecosistemas marinos, ya que ayudan a mantener los lechos de pastos marinos y los arrecifes de coral. Tienen una vida larga y viajan grandes distancias entre sus sitios de alimentación y anidación.'
+   },
+   {
+       'id': 'terrestre-caballo',
+       'name': 'Caballo',
+       'habitat': 'Terrestre',
+       'species': 'Equus ferus caballus',
+       'description': 'Los caballos son mamíferos grandes y herbívoros que han sido domesticados por humanos. Son conocidos por su velocidad, fuerza y uso en el transporte y la agricultura. Los caballos tienen una larga historia de colaboración con los humanos, desde su uso en la guerra hasta su papel en el trabajo agrícola y el transporte. Son animales sociales que forman fuertes lazos tanto con otros caballos como con sus cuidadores humanos.'
+   },
+   {
+       'id': 'terrestre-cacatua',
+       'name': 'Cacatúa',
+       'habitat': 'Terrestre',
+       'species': 'Cacatuidae',
+       'description': 'Las cacatúas son aves conocidas por su plumaje colorido y su capacidad para imitar sonidos. Viven en hábitats variados, desde bosques tropicales hasta zonas semiáridas. Las cacatúas son aves muy inteligentes y sociales, a menudo formando grandes bandadas. Son apreciadas como mascotas debido a su capacidad para aprender trucos y su interacción con los humanos.'
+   },
+   {
+       'id': 'terrestre-chango',
+       'name': 'Mono',
+       'habitat': 'Terrestre',
+       'species': 'Simia',
+       'description': 'Los monos son primates conocidos por su inteligencia y habilidades sociales. Viven en diversas regiones, desde selvas tropicales hasta sabanas. Los monos son muy adaptables y viven en una amplia variedad de hábitats. Tienen comportamientos sociales complejos y utilizan herramientas para conseguir alimentos. Son importantes para los ecosistemas por su papel en la dispersión de semillas y la polinización.'
+   },
+   {
+       'id': 'terrestre-dromedario',
+       'name': 'Dromedario',
+       'habitat': 'Terrestre',
+       'species': 'Camelus dromedarius',
+       'description': 'Los dromedarios son mamíferos grandes adaptados a vivir en desiertos. Son conocidos por su joroba única, que utilizan para almacenar grasa y agua. Los dromedarios son esenciales para la vida en el desierto, ya que proporcionan transporte y recursos como leche y carne. Son animales muy resistentes y pueden sobrevivir largos periodos sin agua.'
+   },
+   {
+       'id': 'terrestre-jirafa',
+       'name': 'Jirafa',
+       'habitat': 'Terrestre',
+       'species': 'Giraffa',
+       'description': 'Las jirafas son mamíferos altos y herbívoros conocidos por su largo cuello y patas. Habitan en sabanas y bosques abiertos en África. Las jirafas son únicas por su estructura física, que les permite alcanzar hojas altas en los árboles que otros animales no pueden. Son importantes para el ecosistema, ya que ayudan a podar árboles y a dispersar semillas.'
+   },
+   {
+       'id': 'terrestre-koala',
+       'name': 'Koala',
+       'habitat': 'Terrestre',
+       'species': 'Phascolarctos cinereus',
+       'description': 'Los koalas son marsupiales herbívoros que viven en bosques de eucalipto en Australia. Son conocidos por su apariencia adorable y su estilo de vida arbóreo. Los koalas pasan la mayor parte de su vida en los árboles, alimentándose casi exclusivamente de hojas de eucalipto. Son animales solitarios y tienen un sistema digestivo especializado para desintoxicar las hojas de eucalipto que comen.'
+   },
+   {
+       'id': 'terrestre-marmota',
+       'name': 'Marmota',
+       'habitat': 'Terrestre',
+       'species': 'Marmota',
+       'description': 'Las marmotas son roedores grandes que viven en madrigueras en áreas montañosas y de pradera. Son conocidas por su comportamiento hibernante y sus llamadas de alerta. Las marmotas son animales sociales que viven en grupos familiares y tienen complejas madrigueras subterráneas. Son importantes para el ecosistema al airear el suelo y ayudar en la dispersión de semillas.'
+   },
+   {
+       'id': 'terrestre-panda',
+       'name': 'Panda',
+       'habitat': 'Terrestre',
+       'species': 'Ailuropoda melanoleuca',
+       'description': 'Los pandas son mamíferos grandes y herbívoros conocidos por su pelaje blanco y negro. Viven en bosques de bambú en China y son considerados una especie en peligro de extinción. Los pandas tienen una dieta especializada en bambú, lo que los hace vulnerables a la pérdida de hábitat. Son un símbolo de conservación y han ayudado a aumentar la conciencia sobre la necesidad de proteger la biodiversidad.'
+   },
+   {
+       'id': 'terrestre-pantera',
+       'name': 'Pantera',
+       'habitat': 'Terrestre',
+       'species': 'Panthera',
+       'description': 'Las panteras son grandes felinos conocidos por su fuerza y agilidad. Habitan en diversos hábitats, desde selvas tropicales hasta bosques y montañas. Las panteras son depredadores solitarios y nocturnos que tienen una gran capacidad para acechar y capturar a sus presas. Son importantes para mantener el equilibrio de los ecosistemas al controlar las poblaciones de otros animales.'
+   },
+   {
+       'id': 'terrestre-venado',
+       'name': 'Venado',
+       'habitat': 'Terrestre',
+       'species': 'Cervidae',
+       'description': 'Los venados son mamíferos herbívoros que viven en bosques y praderas. Son conocidos por sus astas, que los machos utilizan durante la época de apareamiento para competir por las hembras. Los venados son animales adaptables que pueden vivir en una variedad de hábitats. Son importantes para el ecosistema al dispersar semillas y servir como presa para depredadores.'
+   }
+];
